16949b4352af1b76330a09c656c1967096566657
Parth-Prajapati18 <parthb1806@gmail.com>
2023-05-18 22:30:44 -0400

    Starting new Project

---
+++ parth-ecom/app/page.js
@@ -0,0 +1,14 @@
+import './globals.css'
+
+export const  metadata = {
+  title: 'PartheComm.com - Wear the code'
+}
+
+export default function Home() {
+  return (
+    <div>
+      Hey! This is PartheComm 
+      <div className="mx-4 bg-slate-200">This is Parth</div>
+    </div>
+  )
+}
