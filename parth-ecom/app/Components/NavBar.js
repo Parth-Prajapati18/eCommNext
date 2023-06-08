2d1ca6ee25c2c5456518e07d5f49e5621b875c5a
Parth-Prajapati18 <parthb1806@gmail.com>
2023-06-07 22:02:19 -0400

    home local

--- parth-ecom/app/Components/NavBar.js
+++ parth-ecom/app/Components/NavBar.js
@@ -6,11 +6,43 @@ import { RxHamburgerMenu } from 'react-icons/rx'
 import { MdArrowDropUp, MdArrowDropDown } from 'react-icons/md'
 import { useState } from 'react'
 import Link from 'next/link'
+import {connection} from '../lib/db.js'
+
 
 function navbar() {
 
+  const [email, setEmail] = useState('');
+  const [password, setPassoerd] = useState('');
+
   const [isArrowUp, setIsArrowUp] = useState(false)
   const [ isClicked , setIsClicked ] = useState(false)
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    
+    connection.query(
+      'SELECT * FROM user_login WHERE email = ?', [email],
+      (err, results) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        if (results.length > 0 ) {
+          const user = results[0];
+
+          if ( password === user.password) {
+            alert('Login Successful');
+          } else {
+            alert("Incorrect Password");
+
+          }
+        } else {
+          alert('User not Found');
+        }
+      }
+    );
+  };
 
   return (
     <>
@@ -184,18 +216,18 @@ function navbar() {
                 </div>
 
                 <div className='w-full'>
-                  <form>
+                  <form onSubmit={handleLogin}>
                     <div className='mb-4'>
                       <label className='block text-gray-700 text-sm font-bold mb-2' htmlfor="username">
                         Username
                       </label>
-                      <input className='shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' id="username" type="text" placeholder='Username' />
+                      <input className='shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' id="username" type="text" value={email} placeholder='Username' />
                     </div>
                     <div className='mb-6'>
                       <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='password'>
                         Password
                       </label>
-                      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+                      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" value={password} placeholder="******************" />
                     </div>
                     <div className='flex items-center justify-between'>
                       <button class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
---
+++ parth-ecom/app/lib/db.js
@@ -0,0 +1,8 @@
+import { createConnection } from 'mysql2';
+
+const connection = createConnection({
+    host: '54.39.132.104',
+    user: 'devparthp_myApp_db',
+    password: 'B7KyDa%HrMu^2*hL@pIljxIT@k5v5ig&6Y#p$JsFKG#Bu2*WQOx&*Sb&QC#X2B0s73',
+    database: 'devparthp_myApp',        
+});
