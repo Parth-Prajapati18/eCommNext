161d6cf5f486b7ab07e763459549889e5afa339b
Parthkumar <parthb1806@gmail.com>
2023-06-23 18:27:22 -0400

    optimizing Navbar

--- parth-ecom/pages/api/login.js
+++ parth-ecom/pages/api/login.js
@@ -1,6 +1,15 @@
 import { connection } from "./lib/db";
+import bcrypt from 'bcrypt';
+import Jwt  from "jsonwebtoken";
 
 export default function handler(req,res){
+
+  if (req.method === 'POST') {
+
+    const { username, password} = req.body; 
+
+  }
+
 
     const { username, password} = req.body; 
     const query = 'SELECT user_email, user_password FROM user_login WHERE user_email=? AND user_password=?' ;
